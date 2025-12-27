ad276afa0b8b4e120a75faa5ff217c600a9d6244
Tony <84645195+t5188@users.noreply.github.com>
2025-12-27 13:34:57 +0800

    更新 StreamProxy.sh

--- StreamProxy.sh
+++ StreamProxy.sh
@@ -1,3 +1,15 @@
 #!/system/bin/sh
 scripts_dir="/data/adb/StreamProxy/scripts"
-(until [ "$(getprop sys.boot_completed)" -eq 1 ]; do sleep 3; done; "${scripts_dir}/start.sh") &
+mode=$(stat -c %a "${scripts_dir}/start.sh" 2>/dev/null)
+
+(
+  until [ "$(getprop sys.boot_completed)" = "1" ]; do sleep 3; done
+
+  if [ "${mode}" != "755" ]; then
+    chmod 755 "${scripts_dir}/start.sh"
+    chown root:net_admin "${scripts_dir}/start.sh"
+  fi
+
+  "${scripts_dir}/start.sh"
+) &
+exit 0
